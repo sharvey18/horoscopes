476c68ddcb419242bedfbfba3861d6522185f22d
Sam Harvey <s.rharveyiv@gmail.com>
2017-07-30 11:57:05 -0700

    Weather App
    Weather Wizard

--- public/index.php
+++ public/index.php
@@ -1,14 +1,31 @@
-<!DOCTYPE html>
+
 <!DOCTYPE html>
 <html>
   <head>
-    <meta charset="utf-8">
-    <title>NFL API</title>
+    <title>Horoscope Teller</title>
     <link rel="stylesheet" href="sass/app.min.css">
   </head>
   <body>
 
-    <h1>NFL API</h1>
+    <!-- <div>
+      <p class="greeting-id">The ID is </p>
+      <p class="greeting-content">The content is </p>
+    </div> -->
 
+    <h1>Weather Wizard</h1>
+    <span id="error"></span>
+
+    <div class="container">
+      <input type="text" name="city" id="city" placeholder="City Name">
+      <button type="button" name="button" id="submitWeather">Search City</button>
+    </div>
+
+    <div id="show">
+
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" charset="utf-8"></script>
+    <script src="js/scripts.js" charset="utf-8"></script>
   </body>
 </html>
